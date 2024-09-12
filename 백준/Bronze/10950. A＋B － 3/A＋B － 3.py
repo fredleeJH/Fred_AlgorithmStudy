43a5c928ab4270928a9e68721a51b702cb216c2c
fredleeJH <155813460+fredleeJH@users.noreply.github.com>
2024-09-13 02:57:01 +0900

    [Bronze V] Title: A+B - 3, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -0,0 +1,8 @@
+# A+B-3 10950
+count = int(input())
+for i in range(count):
+    a, b = map(int, input().split())
+    sum = a + b
+    print(a+b)
+    
+#만약에 5번을 계산하고 한 번에 결과 값을 출력하려면 리스트를 써야할 거 같았다.
