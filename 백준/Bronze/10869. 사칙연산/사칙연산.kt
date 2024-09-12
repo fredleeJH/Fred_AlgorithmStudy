353fb4e18e4e97c3bae6b5a26cfcd3f91dc9ccdd
fredleeJH <155813460+fredleeJH@users.noreply.github.com>
2024-09-13 02:56:40 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 13296 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,17 @@
+package com.example.basicsyntax
+
+import java.util.Scanner
+
+fun main() {
+   val sc = Scanner(System.`in`)
+    val value1 = sc.nextInt()
+    val value2 = sc.nextInt()
+
+    println(value1+value2)
+    println(value1-value2)
+    println(value1*value2)
+    println(value1/value2)
+    println(value1%value2)
+
+
+}
