29a48ce98ecf4c97c9c5f801fafce2b21958a533
JongHoonLee <155813460+fredleeJH@users.noreply.github.com>
2025-04-29 17:31:35 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 964 ms, Memory: 301140 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val num = br.readLine().toInt()
+    for (i in 0..num-1){
+        val arr = br.readLine().split(" ").map { it.toInt() }
+        bw.write("${arr[0]+arr[1]}")
+        bw.newLine()
+
+    }
+    bw.flush()
+}
