10928ff64117158af261e5e4694f17432e95723a
JongHunLee <155813460+fredleeJH@users.noreply.github.com>
2025-11-24 19:00:49 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 96 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinstudy
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val num = br.readLine().toInt()
+    for (i in 1..num) {
+        bw.write("*".repeat(i))
+        bw.newLine()
+    }
+
+    bw.flush()
+    bw.close()
+}
