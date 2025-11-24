2e6c2cf8e8222b82bf49d0aef3d85d6c8f2a644f
JongHunLee <155813460+fredleeJH@users.noreply.github.com>
2025-11-24 18:51:03 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 112 ms, Memory: 21348 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinstudy
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (a, b) = br.readLine().split(" ").map { it.toInt() }
+    if (a > b)
+        bw.write(">")
+    else if (a < b)
+        bw.write("<")
+    else
+        bw.write("==")
+    bw.flush()
+    bw.close()
+}
